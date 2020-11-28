3d24918f027be597da638e98b4f789737e1b5b7f
NoraTomas <nora.tomas@vipps.no>
2020-11-28 19:04:24 +0100

    Add Oslo weather data description

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const express = require("express")
 const https = require("https")
-const dotenv = require("dotenv")
+const dotenv = require("dotenv");
+const { response } = require("express");
 
 dotenv.config()
 const app = express();
 
-app.get("/", function (req, res) {
-    https.get()
+const url = `https://api.openweathermap.org/data/2.5/weather?q=Oslo&appid=${process.env.API_KEY}`
 
-    res.send("Server is up and running")
+app.get("/", function (req, res) {
+    https.get(url, function (response) {
+        console.log(response.statusCode)
+
+        response.on("data", function (data) {
+            const weatherData = JSON.parse(data)
+            console.log(weatherData.weather[0].description)
+        })
+    })
+    
 })
 
 
