6c82ff88e71f2de25a0f870ab08a3370e46fe2a6
NoraTomas <nora.tomas@vipps.no>
2020-11-29 20:55:52 +0100

    Show correct character type

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express")
 const https = require("https")
 const dotenv = require("dotenv");
-const { response } = require("express");
 const bodyParser = require("body-parser")
 
 dotenv.config()
@@ -20,21 +19,31 @@ app.get("/", function (req, res) {
 app.post("/", function (req, res) {
     const city = req.body.cityName
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}&units=${unit}`
+    res.set({ 'content-type': 'charset=utf-8' });
 
     https.get(url, function (response) {
         console.log(response.statusCode)
 
-        response.on("data", function (data) {
-            const weatherData = JSON.parse(data)
-            const weatherDescription = weatherData.weather[0].description
-            const icon = weatherData.weather[0].icon
-            const imageURL = `http://openweathermap.org/img/w/${icon}.png`
-            res.write(`<p> The temperature in ${city} is: ${weatherData.main.temp} degrees! </p> `)
-            res.write(`<img src="${imageURL}">`)
+        if(response.statusCode == "404"){
+            res.write(`<h1>Sorry, the city you entered does not exist! </h1>`)
             res.send()
-        }) 
+        }
+
+        if(response.statusCode == "200"){
+            response.on("data", function (data) {
+                const weatherData = JSON.parse(data)
+                const icon = weatherData.weather[0].icon
+                const imageURL = `http://openweathermap.org/img/w/${icon}.png`
+                res.set({ 'content-type': 'charset=utf-8' });
+                res.write(`<head> <meta charset="utf-8"> </head> `)
+                res.write(`<p> The temperature in ${city} is: ${weatherData.main.temp} degrees! </p> `)
+                res.write(`<img src="${imageURL}">`)
+                res.send()
+                
+            }) 
+        }
     })
-    console.log(req.body.cityName)
+    console.log("The city name is: " + req.body.cityName)
 })
 
 
